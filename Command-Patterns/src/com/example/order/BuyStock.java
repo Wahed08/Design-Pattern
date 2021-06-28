7e9dd835a1458ba16e7e3eb954df235419214323
Wahed08 <wahedfarhad123@gmail.com>
2021-06-28 13:03:32 +0600

    Command-Patterns

---
+++ Command-Patterns/src/com/example/order/BuyStock.java
@@ -0,0 +1,14 @@
+package com.example.order;
+
+public class BuyStock implements Order {
+
+    private Stock abcStock;
+
+    public BuyStock(Stock abcStock){
+        this.abcStock = abcStock;
+    }
+
+    public void execute() {
+        abcStock.buy();
+    }
+}
---
+++ Command-Patterns/src/com/example/order/SellStock.java
@@ -0,0 +1,14 @@
+package com.example.order;
+
+public class SellStock implements Order{
+
+    private Stock abcStock;
+
+    public SellStock(Stock abcStock){
+        this.abcStock = abcStock;
+    }
+
+    public void execute() {
+        abcStock.sell();
+    }
+}
